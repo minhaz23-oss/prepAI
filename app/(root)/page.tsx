133df5b6c8820522f03f280f37881bdaf67cfa50
Minhaz Fardin <minhazfardin25@gmail.com>
2025-05-01 11:30:19 +0600

    showing the interviews in the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,49 +1,73 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
-import { dummyInterviews } from '@/constants'
-import InterviewCard from '@/components/InterviewCard'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { dummyInterviews } from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
+import {
+  getCurrentUser,
+  getInterviewByUserId,
+  getLatesInterviews,
+} from "@/lib/actions/auth.action";
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewByUserId(user?.id!),
+    await getLatesInterviews({ userId: user?.id! }),
+  ]);
+
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = latestInterviews?.length! > 0;
+
   return (
     <>
-      <section className=' card-cta'>
-        <div className=' flex flex-col gap-6 max-w-lg'>
-          <h2>Get interview-ready with AI porwered practice and  feedback</h2>
-          <p className=' text-lg'>Practice on real world interview question and get instant feedback</p>
-          <Button asChild className='btn-primary max-sm:w-full'>
-            <Link href='/interview'>Start an Interview</Link>
+      <section className=" card-cta">
+        <div className=" flex flex-col gap-6 max-w-lg">
+          <h2>Get interview-ready with AI porwered practice and feedback</h2>
+          <p className=" text-lg">
+            Practice on real world interview question and get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
           </Button>
         </div>
         <Image
-          src='/robot.png'
-          alt='hero'
+          src="/robot.png"
+          alt="hero"
           width={400}
           height={400}
-          className=' max-sm:hidden'
-          />
+          className=" max-sm:hidden"
+        />
       </section>
-      <section className=' flex flex-col gap-6 mt-8 '>
-          <h2>Your interviews</h2>
-          <div className=' interviews-section'>
-            {dummyInterviews.map((interview) => (
-              <InterviewCard key={interview.id} {...interview}/>
-            ))}
-            {/* <p>You have'nt taken any interviews yet</p> */}
-          </div>
+      <section className=" flex flex-col gap-6 mt-8 ">
+        <h2>Your interviews</h2>
+        <div className=" interviews-section">
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard key={interview.id} {...interview} />
+            ))
+          ) : (
+            
+            <p>You have'nt taken any interviews yet</p>
+          )}
+        </div>
       </section>
-      <section className=' flex flex-col gap-6 mt-8'>
+      <section className=" flex flex-col gap-6 mt-8">
         <h2>Take an interview</h2>
-        <div className=' interviews-section'>
-        {dummyInterviews.map((interview) => (
-              <InterviewCard key={interview.id} {...interview}/>
-            ))}
-          {/* <p>There are no interviews available</p> */}
+        <div className=" interviews-section">
+          {hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard key={interview.id} {...interview} />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -2,7 +2,7 @@
 import { auth, db } from "@/firebase/admin";
 import { cookies } from "next/headers";
 
-const ONE_WEEK=60 * 60 * 24 * 7 * 1000;
+const ONE_WEEK = 60 * 60 * 24 * 7 * 1000;
 
 export async function signUp(params: SignUpParams) {
   const { uid, email, name } = params;
@@ -21,10 +21,10 @@ export async function signUp(params: SignUpParams) {
       name,
     });
 
-    return{
-      success:true,
+    return {
+      success: true,
       message: "Account created successfully. please sign in",
-    }
+    };
   } catch (e: any) {
     console.log(e);
     if (e.code === "autnh/email-already-exists") {
@@ -40,23 +40,23 @@ export async function signUp(params: SignUpParams) {
   }
 }
 
-export async function setSessionCookie(idToken: string){
-   const cookieStore = await cookies();
-   const sessionCookie = await auth.createSessionCookie(idToken,{
-    expiresIn: ONE_WEEK, 
-   })
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK,
+  });
 
-   cookieStore.set('session', sessionCookie,{
+  cookieStore.set("session", sessionCookie, {
     maxAge: ONE_WEEK,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     path: "/",
     sameSite: "lax",
-   })
+  });
 }
 
 export async function signIn(params: SignInParams) {
-  const {email,idToken } = params;
+  const { email, idToken } = params;
   try {
     const useRecord = await auth.getUserByEmail(email);
     if (!useRecord) {
@@ -67,7 +67,7 @@ export async function signIn(params: SignInParams) {
     }
     await setSessionCookie(idToken);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return {
       success: false,
       message: "Failed to sign in",
@@ -76,28 +76,62 @@ export async function signIn(params: SignInParams) {
 }
 
 export async function getCurrentUser(): Promise<User | null> {
-    const cookieStore = await cookies();
-    const sessionCookie = cookieStore.get('session')?.value;
-    if(!sessionCookie) return null;
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
 
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
 
-    try {
-      const decodedClaims = await auth.verifySessionCookie(sessionCookie,true);
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
 
-      const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
-      if(!userRecord.exists) return null;
-
-      return {
-        ...userRecord.data(),
-        id: userRecord.id,
-      } as User;
-    } catch (error) {
-      console.log(error)
-      return null
-    }
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
 
-export async function isAurhenticated(){
+export async function isAurhenticated() {
   const user = await getCurrentUser();
   return !!user;
+}
+
+export async function getInterviewByUserId(
+  userId: string
+): Promise<Interview[] | null> {
+  const interviews = await db
+    .collection("interviews")
+    .where("userId", "==", userId)
+    .orderBy("createdAt", "desc")
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
+export async function getLatesInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 20 } = params;
+  const interviews = await db
+    .collection("interviews")
+    .orderBy("createdAt", "desc")
+    .where('finalized', '==', true)
+    .where("userId", "!=", userId)
+    .limit(limit)
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
 }
